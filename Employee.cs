eb9e8fd38789e070428c7a521cd9f676ec43fed3
flydge <followflydge@gmail.com>
2018-01-03 22:02:06 +0300

    Introduction to C#

---
+++ Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task6
+{
+    public class Employee
+    {
+        private string name;
+        private string position;
+        private int salaryValue;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, string position, int salaryValue)
+        {
+            this.name = name;
+            this.position = position;
+            this.salaryValue = salaryValue;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Position
+        {
+            get => position;
+            set => position = value;
+        }
+
+        public int SalaryValue
+        {
+            get => salaryValue;
+            set => salaryValue = value;
+        }
+
+        public static int CompareBySalary(Employee em1, Employee em2)
+        {
+            return em1.salaryValue - em2.salaryValue;
+        }
+        
+        public static int CompareByName(Employee em1, Employee em2)
+        {
+            return String.Compare( em1.name, em2.name, StringComparison.Ordinal);
+        }
+        
+        public static int CompareByPosition(Employee em1, Employee em2)
+        {
+            return String.Compare( em1.position, em2.position, StringComparison.Ordinal);
+        }
+        
+        
+        
+
+        public override string ToString()
+        {
+            return String.Format("name:{0,-15} position:{1,-15} salary:{2,-10}",name,position,salaryValue);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            List<Employee> listOfEmployees = new List<Employee>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                
+                listOfEmployees.Add(new Employee("Name"+i,"Worker",200+i));
+                listOfEmployees.Add(new Employee("Name"+(i^2+i),"SuperWorker",200+2*i));
+                
+            }
+            
+            listOfEmployees.Sort(Employee.CompareByPosition);    //compare by position
+            // listOfEmployees.Sort(Employee.CompareBySalary); //compare by solary value
+            //listOfEmployees.Sort(Employee.CompareByName);    //compare by name
+
+         
+            foreach (var employee in listOfEmployees)
+            {
+                Console.WriteLine(employee);
+            }
+
+
+
+            
+
+        }
+    }
+}
